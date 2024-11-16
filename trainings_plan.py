a079059e900253ce7e4199146a4364c762ba3664
rneher <robert.neher@bartscher-hasenaecker.de>
2024-11-16 17:49:47 +0100

    exposure of station details

--- trainings_plan.py
+++ trainings_plan.py
@@ -2,18 +2,12 @@ from plan import Plan
 from preferences import Preferences
 
 from flet import (
-    Column,
-    DataCell,
-    DataColumn,
-    DataRow,
-    DataTable,
-    Divider,
+    Container,
     FontWeight,
-    MainAxisAlignment,
+    Page,
     Tab,
+    Tabs,
     Text,
-    TextStyle,
-    colors
 )
 def trainingPlan(validFrom: str) -> list:
     prefs = Preferences()
@@ -24,55 +18,11 @@ def trainingPlan(validFrom: str) -> list:
         stationTabs.append(
                 Tab(
                     tab_content=Text(station["machine_id"],
-                                        weight=FontWeight.BOLD,
-                                        bgcolor=colors.AMBER_400,
-                                        color=colors.BLACK,
+                                        weight=FontWeight.BOLD
                                 ),
-                    content=Column(
-                        alignment=MainAxisAlignment.START,
-                        controls=[
-                            Text(station["comments"]),
-                            Divider(color=colors.AMBER_400),
-                            DataTable(
-                                heading_text_style=TextStyle(
-                                    weight=FontWeight.BOLD,
-                                    color=colors.BLACK,
-                                    size=16
-                                ),
-                                columns=[
-                                    DataColumn(label=Text("Einstellung")),
-                                    DataColumn(label=Text("Wert"))
-                                ],
-                                rows=[
-                                    DataRow(
-                                        cells=[
-                                            DataCell(content=Text("Sätze"),
-                                            ),
-                                            DataCell(content=Text(station["sets"])
-                                            )
-                                        ]
-                                    ),
-                                    DataRow(
-                                        cells=[
-                                            DataCell(content=Text("Wiederholungen"),
-                                            ),
-                                            DataCell(content=Text(station["repeats"])
-                                            )
-                                        ]
-                                    ),
-                                    DataRow(
-                                        cells=[
-                                            DataCell(content=Text("Pause"),
-                                            ),
-                                            DataCell(content=Text(station["break"])
-                                            )
-                                        ]
-                                    )
-                                ]
-                            )
-                        ]
-                    )
-            )
+                    content=Text(station["machine_comments"])
+                    # content=getMachineDetail(station)
+                )
         )
 
     return stationTabs
