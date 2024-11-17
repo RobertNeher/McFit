0d52ee6856ca9bedee76e2c3ec555fbce33e67b3
rneher <robert.neher@bartscher-hasenaecker.de>
2024-11-17 07:20:49 +0100

    plan values revisited

--- persistence.py
+++ persistence.py
@@ -30,9 +30,6 @@ class DBConnection:
                                         """)
                 connection.execute(f"""DROP TABLE IF EXISTS {PLAN_TABLE}
                                         """)
-                connection.execute(f"""DROP TABLE IF EXISTS {RESULT_TABLE}
-                                        """)
-
                 connection.execute(f"""CREATE TABLE {PREFERENCE_TABLE} (
                                         customer_id REFERENCES {CUSTOMER_TABLE} (customer_id),
                                         studio TEXT NOT NULL,
@@ -53,30 +50,20 @@ class DBConnection:
                                         (customer_id REFERENCES {CUSTOMER_TABLE}(customer_id),
                                         valid_from TEXT NOT NULL,
                                         machine_id REFERENCES {MACHINE_TABLE}(name),
-                                        machine_parameters TEXT,
+                                        parameters TEXT,
                                         sets INTEGER,
                                         repeats INTEGER,
                                         break INTEGER,
-                                        machine_comments TEXT);
+                                        comments TEXT);
                                     """)
                 connection.execute(f"""CREATE UNIQUE INDEX PLAN1 ON {PLAN_TABLE}
                                         (customer_id, valid_from DESC, machine_id);""")
-                connection.execute(f"""CREATE TABLE {RESULT_TABLE}
-                                        (customer_id REFERENCES {CUSTOMER_TABLE}(customer_id),
-                                        training_date TEXT NOT NULL,
-                                        machine_id REFERENCES {MACHINE_TABLE}(name),
-                                        weight REAL NOT NULL);
-                                    """)
-                connection.execute(f"""CREATE UNIQUE INDEX RESULT1 ON {RESULT_TABLE}
-                                        (customer_id, training_date, machine_id);
-                                    """)
 
                 connection.commit()
 
                 self.initialize_preferences()
                 self.initialize_machines()
                 self.initialize_plans()
-                # self.initialize_results()
 
     def initialize_preferences(self) -> None:
         settings_init_file = os.path.join(DATASETS_FOLDER, PREFERENCE_TABLE + ".json")
@@ -100,7 +87,7 @@ class DBConnection:
             }
 
     def initialize_machines(self) -> None:
-        logo_file = open(f"{IMAGE_FOLDER}\\McFit-weisserHG.png", "rb")
+        logo_file = open(f"{IMAGE_FOLDER}\\McFit-weisserHG-hochkant.png", "rb")
 
         machine_init_file = os.path.join(DATASETS_FOLDER, MACHINE_TABLE + ".json")
 
@@ -133,8 +120,8 @@ class DBConnection:
                     blob_data
                 )
 
-                # self.connection.execute(insert_query, data_tuple)
                 self.cursor.execute(SQL, data_tuple)
+
             self.connection.commit()
 
     def initialize_plans(self) -> None:
@@ -149,66 +136,24 @@ class DBConnection:
                                                 customer_id,
                                                 valid_from,
                                                 machine_id,
-                                                machine_parameters,
-                                                machine_comments
+                                                parameters,
+                                                sets,
+                                                repeats,
+                                                break,
+                                                comments
                                             )
                                             VALUES (
                                                 "{plan['customer_id']}",
                                                 "{plan['valid_from']}",
                                                 "{machine['machine_id']}",
                                                 "{machine['parameter_values']}",
+                                                "{machine['sets']}",
+                                                "{machine['repeats']}",
+                                                "{machine['break']}",
                                                 "{machine['comments']}"
                                             )""")
 
             self.connection.commit()
-
-    def initialize_results(self) -> None:
-        result_init_file = os.path.join(DATASETS_FOLDER, RESULT_TABLE + ".json")
-        SQL = f"""INSERT INTO {RESULT_TABLE} (
-                    customer_id,
-                    training_date,
-                    machine_id,
-                    weight
-                )
-                VALUES (?, ?, ?, ?)"""
-        if os.path.isfile(result_init_file):
-            with open(result_init_file, "r", encoding="UTF-8") as json_file:
-                results = json.load(json_file)
-
-            for result in results["Results"]:
-                customer_id = result["customerID"]
-
-                for training in result["trainings"]:
-                    # training_date = datetime.strptime(training["trainingDate"], "%Y-%m-%d")
-
-                    for result in training["results"]:
-                        result_list = []
-                        result_list.append(customer_id)
-                        result_list.append(training["trainingDate"])
-                        result_list.extend(list(result.values()))
-                        result_list.append(0)
-                        self.cursor.execute(SQL, result_list)
-
-        self.connection.commit()
-
-    def save_result(self, customerID, training_date, machine_id, weight) -> None:
-        training_date = datetime.strptime(training_date, "%Y-%m-%d")
-
-        self.connection.execute(f"""INSERT INTO {RESULT_TABLE} (
-                                    customer_id,
-                                    training_date,
-                                    machine_id,
-                                    weight
-                                )
-                                VALUES (
-                                    {customerID},
-                                    "{training_date.isoformat()}",
-                                    "{machine_id}",
-                                    {weight},
-                                ON CONFLICT REPLACE
-                                )""")
-
-
 
     def __del__(self):
         self.connection.close()
@@ -217,12 +162,4 @@ class DBConnection:
 #------------------------ MAIN ------------------------#
 if __name__ == "__main__":
     db = DBConnection(initialize=True)
-
-    # SQL = f"SELECT * FROM {PREFERENCE_TABLE}"
-    # db = DBConnection(initialize=False)
-    # cursor = db.connection.cursor()
-    # rows = cursor.execute(SQL)
-    # result = rows.fetchone()
-    # print(result)
     db.connection.commit()
-
